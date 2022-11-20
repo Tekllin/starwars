716a0c22c9258ed34cfec56029c1780e96af5398
Tekllin <111495074+Tekllin@users.noreply.github.com>
2022-11-20 18:29:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const video = document.getElementById('video');
+const deathstar = document.getElementById('fond');
+
+
+
+function play()
+{
+    if (video.ended = true) 
+    {
+        deathstar.style.scale = '1';
+    }
+    else
+    {
+        deathstar.style.scale = '0';
+    }
+};
